ee9830dac7df7a16fdad00ffcca342a265c62c23
Dev-cmyser <105227884+Dev-cmyser@users.noreply.github.com>
2026-08-28 22:40:51 +0000

    Deploying to gh-pages from @ b-on-g/bog@444d918fab7b917003b31f752352e7d63eb78d8e 🚀

---
+++ demo/node.d.ts
@@ -0,0 +1,18 @@
+declare let _$_: {
+    new (): {};
+} & typeof globalThis;
+declare class $ extends _$_ {
+}
+declare namespace $ {
+    export type $ = typeof $$;
+    export class $$ extends $ {
+        static $: $;
+    }
+    namespace $$ {
+        type $$ = $;
+    }
+    export {};
+}
+
+export = $;
+//# sourceMappingURL=node.d.ts.map
